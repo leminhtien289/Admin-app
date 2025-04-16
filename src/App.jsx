a0ed17d819cbe6372168b69374fc3d0da6f28bcf
lolicon <leminhtien2892004@gmail.com>
2025-04-16 12:22:07 +0700

    6. React Router - 7. SPA

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import './App.css'
 import Layout from './components/Layout'
 import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <Layout />
-      <Dashboard />
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="teams" element={<div>Teams page coming soon...</div>} />
+        <Route path="analytics" element={<div>Analytics page coming soon...</div>} />
+        <Route path="messages" element={<div>Messages page coming soon...</div>} />
+        <Route path="integrations" element={<div>Integrations page coming soon...</div>} />
+      </Route>
+    </Routes>
   )
 }
 
